56213f006704bba834e289076d117ad65221899a
Alexey Stafeyev <noegon@yandex.ru>
2017-08-09 01:32:55 +0300

    no message

---
+++ Audioplayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Audioplayer
+//
+//  Created by Alexey Stafeyev on 09.08.17.
+//  Copyright © 2017 Alexey Stafeyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
